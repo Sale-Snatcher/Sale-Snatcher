075c4e5234ea995596f5ca3ab9ee8864a8aeded1
helen <helenta000@gmail.com>
2023-10-21 13:21:51 -0700

    backend progress

--- server/server.js
+++ server/server.js
@@ -1,7 +1,25 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const PORT = 3000;  // Is this the port we're on 4 our server??
+const PORT = 3000;  
+
+const db = require('./database/Models.js');
+
+//handle parsing request body
+app.use(express.json());
+
+//define routes -- any request that comes in goes straight to the routes
+//have the request be sent to a wildcard and forward that to the index.html
+app.get('./sellers/addItems', (req, res) => {
+    const { item, description, price } = req.body;
+
+    const text = `
+    INSERT INTO sellers (item, desciption, price)
+    VALUES (${item}, ${description}, ${price})
+    `;
+
+    db.query(text)
+});
 
 
 
